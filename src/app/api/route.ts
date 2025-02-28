0ca02764b0f593c42552b106b53cd3a153d0fc88
johnpflug <johnpflug@outlook.com>
2025-02-28 11:59:20 +0000

    Changes

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,28 +1,25 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     host: 'mxslurp.click',
     port: 2525,
+    secure: false,
     auth: {
         user: 'hedgehogstocks@mailslurp.biz',
-        pass: 'UihFOU7mN0GoCGwk0DuLy5H6wSJVqMie',
-    },
+        pass: 'UihFOU7mN0GoCGwk0DuLy5H6wSJVqMie'
+    }
 });
 
-const sendMail = async (): Promise<void> => {
-    try {
-        const info = await transporter.sendMail({
-            from: 'hedgehogstocks@mailslurp.biz',
-            to: 'hedgehogstocks@mailslurp.biz',
-            subject: 'Hello',
-            text: 'Hello',
-            html: '<b>Hello</b>',
-        });
+export default async function mailerFunction() {
+    const info = await transporter.sendMail({
+        from: 'hedgehogstocks@mailslurp.biz',
+        to: "hedgehogstocks@mailslurp.biz",
+        subject: "Hello",
+        text: "Hello world",
+        html: "<b>Hello world?</b>",
+    });
 
-        console.log('Message sent: %s', info.messageId);
-    } catch (error) {
-        console.error('Error sending email:', error);
-    }
-};
+    console.log("Message sent: %s", info.messageId);
+}
 
-sendMail();
+mailerFunction();
