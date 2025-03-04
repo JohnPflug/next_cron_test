cc9fed94b7d747396eabdc3282e52b6670e753c8
johnpflug <johnpflug@outlook.com>
2025-03-04 15:19:48 +0000

    Should work

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,22 +1,46 @@
-"use server";
+// "use server";
 import { Novu } from "@novu/api";
+// import axios from 'axios';
 
-// Novu:
 const novu = new Novu({
     secretKey: process.env.NEXT_PUBLIC_NOVU_KEY,
 });
 
-export async function sendMessage() {
-
+export async function GET() {
+    const res = await fetch(`https://api.twelvedata.com/quote?symbol=AAPL&apikey=${process.env.NEXT_PUBLIC_APIKEY}`);
+    const data = await res.json()
     const result = await novu.trigger({
         workflowId: "subscription",
         payload: {
-            message: "Hello",
+            message: data,
         },
         to: {
-            subscriberId: process.env.NEXT_PUBLIC_SUBSCRIBERID as string
-        }
+            subscriberId: process.env.NEXT_PUBLIC_SUBSCRIBERID as string,
+        },
     });
+    console.log("Notification sent:", result);
+    return Response.json({ data })
 }
 
-sendMessage();
+
+
+
+
+
+// export default async function GET(stockSymbol: string) {
+//     const config = {
+//         method: 'get',
+//         url: '/quote',
+//         params: {
+//             symbol: stockSymbol,
+//             apikey: process.env.NEXT_PUBLIC_APIKEY as string
+//         }
+//     };
+//     const res = await stockApi(config);
+//     const data = res.data
+//     return Response.json({ data })
+// }
+
+// GET('AAPL');
+
+
