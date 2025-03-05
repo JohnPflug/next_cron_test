f01e3811c9c2c420af59e97f87cc216277b16b98
johnpflug <johnpflug@outlook.com>
2025-03-05 11:12:48 +0000

    This is using Novu

--- src/app/api/cron/novu.ts
+++ src/app/api/cron/novu.ts
@@ -7,9 +7,9 @@ const novu = new Novu({
 export default async function sendEmail() {
 
     const email = await novu.trigger({
-        workflowId: "subscription",
+        workflowId: "cron-test",
         payload: {
-            message: "Hello",
+            message: "This is a test from me!",
         },
         to: {
             subscriberId: process.env.NEXT_PUBLIC_SUBSCRIBERID as string
